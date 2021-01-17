ebe5ace3e4a1cb4052b89e88207698173a903620
manols <manols@manols-MacBook-Pro.local>
2021-01-16 18:16:11 -0600

    udpaed LoginForm Comop to display and handling errors

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,79 +1,91 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { loginUser } from '../lib/auth';
-import Router from 'next/router';
+import React, { Component } from "react";
+import axios from "axios";
+import { loginUser } from "../lib/auth";
+import Router from "next/router";
 
 export class LoginForm extends Component {
-    state = {
-        email: 'Rey.Padberg@karina.biz',
-        password: 'ambrose.net'
-    }
+  state = {
+    email: "Rey.Padberg@karina.biz",
+    password: "ambrose.net",
+    error: "",
+    isLoading: false,
+  };
 
-    handleChange = (event) => {
-        return this.setState({ [event.target.name] : event.target.value })
-    }
+  handleChange = (event) => {
+    return this.setState({ [event.target.name]: event.target.value });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        loginUser(this.state.email, this.state.password)
-            .then(() => Router.push('/profile'));
-    }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ error: "", isLoading: true })
+    loginUser(this.state.email, this.state.password)
+        .then(() =>
+            Router.push("/profile"))
+        .catch(this.showError)
+  };
 
-    render() {
-        const { email, password } = this.state;
-        return (
-            <div className="login-form">
-                <form onSubmit={this.handleSubmit}>
-                    <input 
-                        type="email"
-                        name="email"
-                        value={email}
-                        placeholder="email"
-                        onChange={this.handleChange}>
-                    </input>
-                    <input 
-                        type="password"
-                        name="password"
-                        value={password}
-                        placeholder="password"
-                        onChange={this.handleChange}>
-                    </input>
-                    <button type="submit">Submit</button>
-                </form>
-                <style jsx>{`
-                    .login-form {
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-                        justify-content: center;
-                        height: 100vh;
-                    }
-                    .login-form form {
-                        display: flex;
-                        flex-direction: column;
-                    }
+  showError = (err) => {
+      console.log('err ====', err);
+      const error = err.response && err.response.data || err.message;
+      return this.setState({ error, isLoading: false })
+  }
 
-                    form input {
-                        padding: 0.5em;
-                        margin-bottom: 1em;
-                    }
+  render() {
+    const { email, password, error, isLoading } = this.state;
+    return (
+      <div className='login-form'>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            placeholder='email'
+            onChange={this.handleChange}
+          ></input>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            placeholder='password'
+            onChange={this.handleChange}
+          ></input>
+          <button type='submit' disabled={isLoading}>{isLoading ? 'Submitting...' : 'Submit'}</button>
+        </form>
+        { error && <div>{error}</div>}
+        <style jsx>{`
+          .login-form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+          }
+          .login-form form {
+            display: flex;
+            flex-direction: column;
+          }
 
-                    form input[type:password] {
-                        font-weight: 700;
-                    }
+          form input {
+            padding: 0.5em;
+            margin-bottom: 1em;
+          }
 
-                    form button {
-                        font-size: 1.2rem;
-                        font-weight: bold;
-                        background: #f60;
-                        color: #fff;
-                        padding: 0.3em;
-                        border-radius: 10px;
-                    }
-                `}</style>
-            </div>
-        )
-    }
+          form input[type:password] {
+            font-weight: 700;
+          }
+
+          form button {
+            font-size: 1.2rem;
+            font-weight: bold;
+            background: #f60;
+            color: #fff;
+            padding: 0.3em;
+            border-radius: 10px;
+          }
+        `}</style>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
